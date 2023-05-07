a729b2ddb27f025af396ff2fb0401a9987372faa
Katarzyna <kasiakadziolka@gmail.com>
2023-05-07 10:54:57 +0200

    main components

--- ManageMe/src/app/app.module.ts
+++ ManageMe/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { RegisterComponent } from './register/register.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectComponent } from './project/project.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    RegisterComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    NewProjectComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
